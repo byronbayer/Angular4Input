de1bd380fc8473ea59e73b43763e3c1d695f5c88
Jay Freeman <byronbayer@hotmail.com>
2017-10-03 23:29:04 +0100

    Added Partner details and Summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { CompanyDetailsComponent } from './company-details/company-details.component';
 import { DataService } from './data.service';
 import { SupportingApplicationComponent } from './supporting-application/supporting-application.component';
+import { PartnerDetailsComponent } from './partner-details/partner-details.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyDetailsComponent,
-    SupportingApplicationComponent
+    SupportingApplicationComponent,
+    PartnerDetailsComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
