2770ad008570e134b18c3094ef704360b955c7c8
Jay Freeman <byronbayer@hotmail.com>
2017-10-03 07:58:28 +0100

    Added CompanyDetails Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CompanyDetailsComponent } from './company-details/company-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyDetailsComponent
   ],
   imports: [
     BrowserModule
